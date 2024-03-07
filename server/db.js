360e2e91e4a0cd59448c652a9ce87d6498076f91
steven huynh <steven.h@live.com>
2024-03-06 21:33:34 -0800

    add create table function

--- server/db.js
+++ server/db.js
@@ -1,6 +1,45 @@
 const pg = require("pg");
 const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/acme_store");
 
+// client - a node pg client ✅
+// createTables method - drops and creates the tables for your application
+// createProduct - creates a product in the database and returns the created record
+// createUser - creates a user in the database and returns the created record. The password of the user should be hashed using bcrypt.
+// fetchUsers - returns an array of users in the database
+// fetchProducts - returns an array of products in the database
+// fetchFavorites - returns an array favorites for a user
+// createFavorite - creates a favorite in the database and returns the created record
+// destroyFavorite - deletes a favorite in the database
+
+async function createTable()
+{
+	const sql = `
+		drop table if exists favorite;
+		drop table if exists "user";
+		drop table if exists product;
+
+		create table "user"(
+			id uuid primary key,
+			username varchar(255) not null unique,
+			password varchar(255) not null
+		);
+
+		create table product(
+			id uuid primary key,
+			name varchar(255) not null
+		);
+
+		create table favorite(
+			id uuid primary key,
+			product_id uuid references product(id) not null,
+			user_id uuid references "user"(id) not null,
+			constraint unique_user_product unique (user_id, product_id)
+		);
+	`;
+	await client.query(sql);
+}
+
 module.exports = {
 	client,
+	createTable,
 };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const { client } = require("./db");
+const { client, createTable } = require("./db");
 const app = express();
+
+// GET /api/users - returns array of users
+// GET /api/products - returns an array of products
+// GET /api/users/:id/favorites - returns an array of favorites for a user
+// POST /api/users/:id/favorites - payload: a product_id
+// - returns the created favorite with a status code of 201
+// DELETE /api/users/:userId/favorites/:id - deletes a favorite for a user, returns nothing with a status code of 204
 
 const init = async () =>
 {
 	console.log("connecting to db");
 	await client.connect();
 	console.log("connected to db");
+
+	console.log("creating table");
+	await createTable();
+	console.log("created table");
 
 	const PORT = 3000;
 	app.listen(PORT, () =>
